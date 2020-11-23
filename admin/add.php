901519309513fbc2f783be4c69d5071a908f2d70
roshanB392 <roshanbonde30@gmail.com>
2020-11-23 18:06:46 +0530

    admin panel update

--- admin/add.php
+++ admin/add.php
@@ -62,7 +62,7 @@ if (isset($_POST['but_logout'])) {
                         <i class="fas fa-film" style="background-color: #4547cf"></i>
                     </div>
                     <div class="booking-form-container">
-                        <form action="verify.php" method="POST">
+                        <form action="spot.php" method="POST">
 
 
                             <select name="theatre" required>
@@ -105,15 +105,17 @@ if (isset($_POST['but_logout'])) {
 
                             <input placeholder="Phone Number" type="text" name="pNumber" required>
                             <input placeholder="email" type="email" name="email" required>
+                            <input placeholder="Movie ID" type="text" name="movie_id">
 
-                            <select name="type" name="payment" required>
+                            <!-- <select name="type" name="payment" required>
                                 <option value="" disabled selected>PAYMENT</option>
                                 <option value="CASH">CASH</option>
-                            </select>
-
+                            </select> -->
+                            
+                            <input placeholder="Amount" type="text" name="cash" required>
 
                             <button type="submit" value="submit" name="submit" class="form-btn">ADD ENTRY</button>
-
+                            
                         </form>
                     </div>
                 </div>
