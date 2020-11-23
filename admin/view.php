7175a321aa75c3079552cc76817ba3d5e327fa6f
Vishal yadav <vy140599@gmail.com>
2020-11-23 19:23:37 +0530

    title fix

--- admin/view.php
+++ admin/view.php
@@ -62,7 +62,7 @@ if (isset($_POST['but_logout'])) {
                     <div class="table-title">
                         <div class="row">
                             <div class="col-sm-8">
-                                <h2>Employee <b>Details</b></h2>
+                                <h2>Booking <b>Details</b></h2>
                             </div>
                             <!--<div class="col-sm-4">
                                 <a href='add.php'><button type="button" class="btn btn-info add-new"><i class="fa fa-plus"></i> Add New</button></a>
