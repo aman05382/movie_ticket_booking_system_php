11a44acc34013ed993f28914d62286454c226b79
roshanB392 <roshanbonde30@gmail.com>
2020-11-23 19:53:37 +0530

    minor bug fix

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <div class="admin-section admin-section1">
     <ul>
         <li><i class="fas fa-sliders-h"></i><a href="admin.php">Dashboard </a><i class="fas admin-dropdown fa-chevron-right"></i></li>
@@ -9,15 +8,3 @@
         <li><i class="fas fa-film"></i><a href="contactus.php">User Feedback</a> <i class="fas admin-dropdown fa-chevron-right"></i></a></li>
     </ul>
 </div>
-=======
-<div class="admin-section admin-section1 ">
-            <ul>
-                <li><i class="fas fa-sliders-h"></i><a href="admin.php">Dashboard </a><i class="fas admin-dropdown fa-chevron-right"></i></li>
-                <li><i class="fas fa-ticket-alt"></i><a href="view.php">Bookings</a> <i class="fas admin-dropdown fa-chevron-right"></i></li>
-                
-                <li><i class="fas fa-film"></i><a href="addmovie.php">Movies</a> <i class="fas admin-dropdown fa-chevron-right"></i></a></li>
-                <li><i class="fas fa-plus-circle"></i><a href="add.php">Add entry</a> <i class="fas admin-dropdown fa-chevron-right"></i></a></li>
-                <li><i class="fas fa-id-card"></i><a href="contactus.php">Contactus</a> <i class="fas admin-dropdown fa-chevron-right"></i></a></li>
-            </ul>
-        </div>
->>>>>>> 17407fd54fa2759903ac53072845b9e4ad9eb6da
