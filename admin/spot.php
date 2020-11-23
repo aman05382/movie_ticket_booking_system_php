3a8557c25c61ec7fcbcdb93d32c477deacdffd56
roshanB392 <roshanbonde30@gmail.com>
2020-11-23 18:09:45 +0530

    same

--- admin/spot.php
+++ admin/spot.php
@@ -1,30 +1,30 @@
 <?php
 
-$fname = $_POST['fName'];
-$lname = $_POST['lName'];
-$email = $_POST['email'];
-$mobile = $_POST['pNumber'];
-$theatre = $_POST['theatre'];
-$type = $_POST['type'];
-$date = $_POST['date'];
-$time = $_POST['hour'];
-$movieid = $_POST['movie_id'];
-$amount = $_POST['cash'];
-$order = "ORD" . rand(10000, 99999999);
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cinema_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 
 if(isset($_POST['submit'])) {
-			$servername = "localhost";
-			$username = "root";
-			$password = "";
-			$dbname = "cinema_db";
-
-			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			// Check connection
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			}
-
+	       $fname = $_POST['fName'];
+            $lname = $_POST['lName'];
+            $email = $_POST['email'];
+            $mobile = $_POST['pNumber'];
+            $theatre = $_POST['theatre'];
+            $type = $_POST['type'];
+            $date = $_POST['date'];
+            $time = $_POST['hour'];
+            $movieid = $_POST['movie_id'];
+            $amount = $_POST['cash'];
+            $order = "ORD" . rand(10000, 99999999);
+            
             
 			$qry = "INSERT INTO `bookingtable`(`movieID`, `bookingTheatre`, `bookingType`, `bookingDate`, `bookingTime`, `bookingFName`, `bookingLName`, `bookingPNumber`, `bookingEmail`, `ORDERID`) VALUES  
 			('$movieid', '$theatre', '$type', '$date', '$time', '$fname', '$lname', '$mobile','$email', '$order')";
